e8829a168eca99ddaeb66882663f3de7068ac0d8
Aye Chan Aung Thwin <ayechanaungthwin@gmail.com>
2020-02-03 02:44:54 +0630

    first commit

---
+++ 1DataTypes/index.js
@@ -0,0 +1,115 @@
+/*
+const = declaring final variable with assigned values and that cannot be reassigned later
+let = declaring variable that can be assigned later
+*/
+/*
+number - 1, 1.2, -10
+string - 'Hi', "Hi", `Hi`
+boolean - true, false
+Array - Array<T>
+object - {name:"Aye Chan"}
+tuple - [T, T, T]
+any - *
+*/
+//1. Number
+var num = 1;
+console.log("1. Number");
+console.log("Number is " + num + ".");
+console.log();
+//2. String
+var word = "Hello";
+console.log("2. String");
+console.log("String is outputing " + word + ".");
+console.log();
+//3. Boolean
+var isRunning = true;
+console.log("3. Boolean");
+console.log("Boolean is currently " + isRunning + ".");
+console.log();
+//4. Array<T>
+var animals = ["Cat", "Dog", "Bird"];
+console.log("4. Array<T>");
+console.log("Array index of 0 has \"" + animals[0] + ".\"");
+for (var _i = 0, animals_1 = animals; _i < animals_1.length; _i++) {
+    var animal = animals_1[_i];
+    console.log("Enhanced For-Loop & UpperCase -> " + animal.toUpperCase() + ".");
+}
+console.log();
+//5. Object
+var person = {
+    name: "Aye Chan Aung Thwin",
+    age: 30
+};
+console.log("5. Object");
+console.log("Name is \"" + person.name + "\" and age is \"" + person.age + ".\"");
+console.log();
+//6. Tuple
+console.log("6. Tuple");
+var role = [1, 'Aye Chan Aung Thwin'];
+console.log("Tuple data [0] -> " + role[0] + ".");
+console.log("Tuple data [1] -> " + role[1] + ".");
+console.log();
+//7. Enum
+console.log("7. Enum (Human Readable Form)");
+//Enum Creation
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["MEMBER"] = 1] = "MEMBER";
+})(Role || (Role = {}));
+;
+//User Object
+var user = {
+    status: Role.ADMIN
+};
+user.status = Role.ADMIN; //Assign values
+if (user.status === Role.ADMIN) {
+    console.log("Enum Role is " + Role.ADMIN + " which is ADMIN.");
+}
+else if (user.status === Role.MEMBER) {
+    console.log("Enum Role is " + Role.MEMBER + " which is MEMBER.");
+}
+else {
+    console.log("Enum Role is UNKNOWN.");
+}
+console.log();
+//8. Any
+console.log("8. Any");
+var data;
+data = 1;
+console.log("Data Type is " + typeof (data) + " and value is " + data + ".");
+data = 1.3;
+console.log("Data Type is " + typeof (data) + " and value is " + data + ".");
+data = "Aye Chan Aung Thwin";
+console.log("Data Type is " + typeof (data) + " and value is " + data + ".");
+data = true;
+console.log("Data Type is " + typeof (data) + " and value is " + data + ".");
+data = [1, 2];
+console.log("Data Type is " + typeof (data) + " and value is " + data + ".");
+console.log();
+//9. Enum (Another Approach)
+console.log("9. Enum (Another Approach)");
+var ADMIN = 0;
+var MEMBER = 1;
+var human = ADMIN;
+if (human === ADMIN) {
+    console.log("Enum is " + ADMIN + " which is ADMIN.");
+}
+else if (human === MEMBER) {
+    console.log("Enum is " + MEMBER + " which is MEMBER.");
+}
+else {
+    console.log("Enum is UNKNOWN.");
+}
+console.log();
+/*10. Tuple Array - It looks like MultiDimentional Array in Java
+but difference is that it is data type containing 2 fixed data types with fixed tuple length
+*/
+console.log("10. Tuple with Array");
+var roles = [[1, "HaHa"], [2, "HeHe"], [3, "HoHo"]];
+for (var i = 0; i < roles.length; i++) {
+    for (var j = 0; j < roles[i].length; j++) { //Length of Turple is fixed. j<2
+        console.log("Simple For Loop & Tuple Array [" + i + "][" + j + "] -> " + roles[i][j]);
+    }
+}
+console.log();
---
+++ 1DataTypes/index.ts
@@ -0,0 +1,136 @@
+/*
+const = declaring final variable with assigned values and that cannot be reassigned later
+let = declaring variable that can be assigned later
+*/
+
+/*
+number - 1, 1.2, -10
+string - 'Hi', "Hi", `Hi`
+boolean - true, false
+Array - Array<T>
+object - {name:"Aye Chan"}
+tuple - [T, T, T]
+any - *
+*/
+
+//1. Number
+const num: number = 1;
+
+console.log("1. Number");
+console.log("Number is "+num+".");
+console.log();
+
+//2. String
+const word: string = "Hello";
+
+console.log("2. String");
+console.log("String is outputing "+word+".");
+console.log();
+
+//3. Boolean
+const isRunning: boolean = true;
+
+console.log("3. Boolean");
+console.log("Boolean is currently "+isRunning+".");
+console.log();
+
+//4. Array<T>
+const animals: Array<string> = ["Cat","Dog","Bird"];
+
+console.log("4. Array<T>");
+console.log("Array index of 0 has \""+animals[0]+".\"");
+for (const animal of animals) {
+    console.log("Enhanced For-Loop & UpperCase -> "+animal.toUpperCase()+".");
+}
+console.log();
+
+//5. Object
+const person: {
+    name: string,
+    age: number
+} = {
+    name: "Aye Chan Aung Thwin",
+    age: 30
+};
+console.log("5. Object");
+console.log("Name is \""+person.name+"\" and age is \""+person.age+".\"");
+console.log();
+
+//6. Tuple
+console.log("6. Tuple");
+const role: [number, string] = [1, 'Aye Chan Aung Thwin'];
+console.log("Tuple data [0] -> "+role[0]+".");
+console.log("Tuple data [1] -> "+role[1]+".");
+console.log();
+
+//7. Enum
+console.log("7. Enum (Human Readable Form)");
+//Enum Creation
+enum Role {
+    ADMIN,
+    MEMBER
+};
+
+//User Object
+let user: {
+    status: Role
+} = {
+    status: Role.ADMIN
+}
+
+user.status = Role.ADMIN; //Assign values
+
+if (user.status === Role.ADMIN) {
+    console.log("Enum Role is "+Role.ADMIN+" which is ADMIN.");
+}
+else if (user.status === Role.MEMBER) {
+    console.log("Enum Role is "+Role.MEMBER+" which is MEMBER.");
+}
+else {
+    console.log("Enum Role is UNKNOWN.");
+}
+console.log();
+
+//8. Any
+console.log("8. Any");
+let data: any;
+data = 1;
+console.log("Data Type is "+typeof(data)+" and value is "+data+".");
+data = 1.3;
+console.log("Data Type is "+typeof(data)+" and value is "+data+".");
+data = `Aye Chan Aung Thwin`;
+console.log("Data Type is "+typeof(data)+" and value is "+data+".");
+data = true;
+console.log("Data Type is "+typeof(data)+" and value is "+data+".");
+data = [1 ,2];
+console.log("Data Type is "+typeof(data)+" and value is "+data+".");
+console.log();
+
+//9. Enum (Another Approach)
+console.log("9. Enum (Another Approach)")
+const ADMIN = 0;
+const MEMBER = 1;
+
+const human = ADMIN;
+if (human === ADMIN) {
+    console.log("Enum is "+ADMIN+" which is ADMIN.");
+}
+else if (human === MEMBER) {
+    console.log("Enum is "+MEMBER+" which is MEMBER.");
+}
+else {
+    console.log("Enum is UNKNOWN.");
+}
+console.log();
+
+/*10. Tuple Array - It looks like MultiDimentional Array in Java 
+but difference is that it is data type containing 2 fixed data types with fixed tuple length
+*/
+console.log("10. Tuple with Array");
+const roles: [number, string][] = [[1, "HaHa"],[2, "HeHe"],[3, "HoHo"]];
+for (let i=0; i<roles.length; i++) {
+    for (let j=0; j<roles[i].length; j++) { //Length of Turple is fixed. j<2
+        console.log("Simple For Loop & Tuple Array ["+i+"]["+j+"] -> "+roles[i][j]);
+    }
+}
+console.log();
